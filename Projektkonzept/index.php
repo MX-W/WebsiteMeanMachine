1327e1ccba4ba61324053ec573cdc348d22b4215
solala5 <wendl.max@gmail.com>
2016-12-06 15:50:45 +0100

    pasted in project conception

---
+++ Projektkonzept/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>SemesterProject</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+
+        <h2>Billy Talent</h2>
+
+        <p><h3>Vorteile</h3>
+
+            - Design passt zum letzten Album<br>
+            - Übersichtlich<br>
+            - wichtige Dinge auf der Hauptseite<br>
+        </p>
+
+        <p><h3>Nachteile</h3>
+
+            - Veraltet wirkend<br>
+            - manche Teile schlecht lesbar<br>
+            - einige Teile passen nicht ins responsive Design<br>
+            - Header und Body Teile nicht gut formatiert (werden nicht skaliert)<br>
+        </p>
+
+        <h3>weder gut noch schlecht</h3>
+
+        -shop auf extra domain<br>
+
+        <br>
+        <h2>Iron Maiden</h2>
+
+        <p><h3>Vorteile</h3>
+
+            -wirkt modern(Slideshow, Dropdown)<br>
+            -design passt zur Band<br>
+            -gutes Responsive Design<br>
+        </p>
+
+        <p><h3>Nachteile</h3>
+
+            - Hauptseite wirkt überladen<br>
+        </p>
+
+        
+        <h2>Metallica</h2>
+
+        <p><h3>Vorteile</h3>
+            -modernes Design<br>
+            -https<br>
+            -gutes Responsive Design<br>
+        </p>
+
+        <p><h3>Nachteile</h3>
+
+            -zwar alles wichtige auf Hauptseite wirkt aber überladen<br>
+        </p>
+        <br>
+
+        <p>
+            <h2>Site Map</h2> 
+            <br>
+            <img src='img/Sitemap.jpg' alt='Sitemap'/>
+
+            <br>
+            <br>
+            <h2>Inhalt der einzelnen Seiten</h2>
+            <h3>The Band</h3>
+            Allgemeine Vorstellung der Band, Weiterleitung auf Unterseiten mit Vorstellung der einzelnen Mitglieder
+            
+            <h3>Music</h3>
+            Embedded Soundcloud-Player
+            
+            <h3>Media</h3>
+            Galerie mit Fotos und embedded Youtube-Videos
+            
+            <h3>Tourdates</h3>
+            tabellarische Auflistung der nächsten Auftritte, Weiterleitung zu offizieller 
+            (Facebook-)Veranstaltung, eventuell Verlinkung zu Vorverkaufsstellen von Karten
+            
+            <h3>Shop</h3>
+            Anzeigen der Ware, Benutzung eines Warenkorbes durch einloggen möglich,
+            Formular für Kundendaten, Online-Bestellung möglich
+            
+            <h3>Contact</h3>
+            Formular für direkt Kontakt per Email, Google Maps einbinden, generelle Anzeige von Kontaktdaten
+            
+            <h2>Datenbank</h2><br>
+            <br>
+            Datenbank für Shop und Login evtl speicherbarer Warenkorb.
+            <br>
+            <br>
+        <h2>Seitendesign</h2>
+        <table>
+            <tr>
+                <td><img height='80%' width='100%' src='img/Design1.JPG' alt='Design1'/></td>
+                <td><img height='100%' width='100%' src='img/Design2.png' alt='Design2'/></td>
+                <td><img height='80%' width='100%' src='img/Design3.JPG' alt='Design3'/></td>
+            </tr>
+        </table>
+        
+    </p>
+
+
+
+    <h2>Quellen: </h2>
+
+    <a href='http://billytalent.com/' target="_blank">Billy Talent</a>
+    <a href='http://ironmaiden.com/' target="_blank">Iron Maiden</a>
+    <a href='https://metallica.com/' target="_blank">Metallica</a>
+    <div></div>
+</body>
+</html>
