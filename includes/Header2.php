6a7a3dd6f69621ac828346367550e513196b8d23
solala5 <wendl.max@gmail.com>
2017-01-31 22:28:14 +0100

    95% fixed dropdown with flexbox

--- includes/Header2.php
+++ includes/Header2.php
@@ -3,32 +3,32 @@
 <header class="header">
     
     <nav class="nav">
-        <ul class="flex_container">
-            <li class="flex_item"><a href="index.php">Home</a></li>
-            <li class="flex_item"><a href="index.php?page=band">The Band</a>
-                <ul>
-                    <li><a href="index.php?page=andi">Andi</a></li>
-                    <li><a href="index.php?page=daniel">Daniel</a></li>
-                    <li><a href="index.php?page=max">Max</a></li>
-                    <li><a href="index.php?page=timo">Timo</a></li>
+        <ul class="flex_container_mainmenue">
+            <li class="flex_item_mainmenue"><a class="mainmenue_link" href="index.php">Home</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=band">The Band</a>
+                <ul class="flex_container_submenue">
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=andi">Andi</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=daniel">Daniel</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=max">Max</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=timo">Timo</a></li>
                 </ul>
             </li>
-            <li class="flex_item"><a href="index.php?page=music">Music</a>
-                <ul>
-                    <li><a href="index.php?page=youtube">Youtube</a></li>
-                    <li><a href="index.php?page=soundcloud">Soundcloud</a></li>
-                    <li><a href="index.php?page=spotify">Spotify</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=music">Music</a>
+                <ul class="flex_container_submenue">
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=youtube">Youtube</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=soundcloud">Soundcloud</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=spotify">Spotify</a></li>
                 </ul>
             </li>
-            <li class="flex_item"><a href="index.php?page=media">Media</a>
-                <ul>
-                    <li><a href="index.php?page=photos">Photos</a></li>
-                    <li><a href="index.php?page=video">Video</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=media">Media</a>
+                <ul class="flex_container_submenue">
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=photos">Photos</a></li>
+                    <li class="flex_item_submenue"><a class="submenue_link" href="index.php?page=video">Video</a></li>
                 </ul>
             </li>
-            <li class="flex_item"><a href="index.php?page=tourdates">Tourdates</a></li>
-            <li class="flex_item"><a href="index.php?page=shop">Shop</a></li>
-            <li class="flex_item"><a href="index.php?page=contact">Contact</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=tourdates">Tourdates</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=shop">Shop</a></li>
+            <li class="flex_item_mainmenue"><div class="line"></div><a class="mainmenue_link" href="index.php?page=contact">Contact</a></li>
         </ul>
     </nav>
     
